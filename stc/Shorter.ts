2fe91304ed65e885a0d9c7af0fbc0bec3e178b74
AtsushiYamashita <yays505@gmail.com>
2020-05-01 15:12:08 +0900

    Update declare variation

--- stc/Shorter.ts
+++ stc/Shorter.ts
@@ -1,12 +1,38 @@
-import * as t from "#/Shorter";
+import Dec, { Type as t, Act, Get, Func } from "#/Shorter";
 
-const act: t.Act<string> = msg => { }
-const get: t.Get<number> = _ => 0;
-const proc: t.Process = _ => 0;
+type NN = (n:number)=>number;
 
-const func: t.Func<number, number> = i => i + 1;
-const Merge: t.Merge<number, number, number> = (a, b) => a + b;
+{
 
-const dic_str: t.Dic = { key: "value" };
-const dic_sug: t.Map<number> = { key: 0 };
+    const act: t.Act<string> = msg => { }
+    const get: t.Get<number> = _ => 0;
+    const proc: t.Process = _ => 0;
 
+    const func: t.Func<number, number> = i => i + 1;
+    const Merge: t.Merge<number, number, number> = (a, b) => a + b;
+    const f2:(n:number)=>number = func;
+    const f3:NN = func;
+    const f4:NN = f2;
+
+    const dic_str: t.Dic = { key: "value" };
+    const dic_sug: t.Map<number> = { key: 0 };
+}
+
+{
+    const act: Dec.Act<string> = msg => { }
+    const get: Dec.Get<number> = _ => 0;
+    const func: Dec.Func<number, number> = i => i + 1;
+    const f2:(n:number)=>number = func;
+    const f3:NN = func;
+    const f4:NN = f2;
+
+}
+
+{
+    const act: Act<string> = msg => { }
+    const get: Get<number> = _ => 0;
+    const func: Func<number, number> = i => i + 1;
+    const f2:(n:number)=>number = func;
+    const f3:NN = func;
+    const f4:NN = f2;
+}
