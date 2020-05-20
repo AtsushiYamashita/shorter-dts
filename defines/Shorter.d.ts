2be0ecaa9c3c75f25ca60fa546613fd319aafd64
AtsushiYamashita <yays505@gmail.com>
2020-05-20 16:48:20 +0900

    Add unsafe

--- defines/Shorter.d.ts
+++ defines/Shorter.d.ts
@@ -11,36 +11,44 @@ export declare type KeyValue<K, V> = [K, V];
 export declare type Dic = Map<string>;
 export declare type Set<K, V> = KeyValue<K, V>;
 
+export declare type Unsafe<T> = T | undefined;
 
-export namespace Declare  {
+
+export namespace Declare {
     export declare type Func<A0, R> = (a: A0) => R;
     export declare type Merge<A0, A1, R> = (a: A0, a1: A1) => R;
-    
+
     export declare type Act<A> = Func<A, any>;
     export declare type Get<R> = Func<any, R>;
     export declare type Process = Func<any, any>;
-    
+
     export declare type Map<V> = { [index: string]: V };
     export declare type KeyValue<K, V> = [K, V];
-    
+
     export declare type Dic = Map<string>;
     export declare type Set<K, V> = KeyValue<K, V>;
+    
+    export declare type Unsafe<T> = T | undefined;
+
 }
 
 export namespace Type {
     export type Func<A0, R> = (a: A0) => R;
     export type Merge<A0, A1, R> = (a: A0, a1: A1) => R;
-    
+
     export type Act<A> = Func<A, any>;
     export type Get<R> = Func<any, R>;
     export type Process = Func<any, any>;
-    
+
     export type Map<V> = { [index: string]: V };
     export type KeyValue<K, V> = [K, V];
-    
+
     export type Dic = Map<string>;
     export type Set<K, V> = KeyValue<K, V>;
+
+    export type Unsafe<T> = T | undefined;
+
 }
 
- export default Declare
+export default Declare
 
